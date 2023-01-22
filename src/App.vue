0391ee5531447a810d81f23844899674825b3d86
David fox <davidfox967@yahoo.com>
2023-01-22 01:52:23 -0700

    initital

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+	<h1>Reaction time app</h1>
+	<button @click="start">Play</button>
+</template>
+
+<script>
+export default {
+	name: "app",
+	components: {},
+};
+</script>
+
+<style></style>
